3d53a0f1e64c03fd6b81b5069a70898f1c699213
ZziM <Denis.Adimov@gmail.com>
2020-08-18 19:22:33 +0300

    master branch commit

---
+++ iDoctor/iDoctor.Patient.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iDoctor.Patient.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
